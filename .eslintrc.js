784deaad8c544c09b82fd27ba414fadcad0d9e25
Michael P. Geraci <me@mgeraci.com>
2022-05-05 13:01:59 -0500

    add midi writer package; copy hot cross buns example and add another voice

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
         "ecmaVersion": "latest"
     },
     "rules": {
+        "no-console": 0,
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('hello, world');
+const MidiWriter = require('midi-writer-js');
+const fs = require('fs');
+
+const track1 = new MidiWriter.Track();
+
+track1.addEvent([
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
+  new MidiWriter.NoteEvent({ pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
+], () => ({ sequential: true }));
+
+const track2 = new MidiWriter.Track();
+
+track2.addEvent([
+  new MidiWriter.NoteEvent({ pitch: ['G4', 'F4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4'], duration: '2' }),
+  new MidiWriter.NoteEvent({ pitch: ['G4', 'F4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4'], duration: '2' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'E4', 'E4', 'E4', 'F4', 'F4', 'F4', 'F4'], duration: '8' }),
+  new MidiWriter.NoteEvent({ pitch: ['G4', 'F4'], duration: '4' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4'], duration: '2' }),
+], () => ({ sequential: true }));
+
+const write = new MidiWriter.Writer([track1, track2]);
+
+try {
+  fs.unlinkSync('output.midi');
+} catch { /* noop */ }
+
+fs.writeFile(
+  'output.midi',
+  write.buildFile(),
+  (err) => console.log(err),
+);
