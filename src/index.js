1293e295e6016a8d62fbb8b257ec75c09c8a65fc
Michael P. Geraci <me@mgeraci.com>
2022-05-05 13:19:27 -0500

    add velocity to test

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const track1 = new MidiWriter.Track();
 
 track1.addEvent([
-  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
-  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
-  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
-  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
-  new MidiWriter.NoteEvent({ pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8' }),
-  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4' }),
-  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2' }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4', velocity: 100 }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2', velocity: 60 }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4', velocity: 40 }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2', velocity: 20 }),
+  new MidiWriter.NoteEvent({ pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8', velocity: 10 }),
+  new MidiWriter.NoteEvent({ pitch: ['E4', 'D4'], duration: '4', velocity: 5 }),
+  new MidiWriter.NoteEvent({ pitch: ['C4'], duration: '2', velocity: 1 }),
 ], () => ({ sequential: true }));
 
 const track2 = new MidiWriter.Track();
